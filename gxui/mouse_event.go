93c45169627f45fc2f5daa26f73869bae17f9906
wrzfeiianshen <wrzfeiianshen@126.com>
2018-07-02 10:54:00 +0800

    根据gxui创建而来

---
+++ gxui/mouse_event.go
@@ -0,0 +1,20 @@
+// Copyright 2015 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package gxui
+
+import (
+	"github.com/wrzfeijianshen/gui/gxui/math"
+)
+
+// 事件分开
+type MouseEvent struct {
+	Button           MouseButton
+	State            MouseState
+	Point            math.Point // Local to the event receiver
+	WindowPoint      math.Point
+	Window           Window
+	ScrollX, ScrollY int
+	Modifier         KeyboardModifier
+}
