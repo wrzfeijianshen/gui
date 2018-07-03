d5f5c97a2247183b2f06e34cd36247c58c877ee8
wrzfeiianshen <wrzfeiianshen@126.com>
2018-07-03 14:19:29 +0800

    add

---
+++ samples/01_window/01_hello/main.go
@@ -0,0 +1,16 @@
+// Copyright 2015 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"github.com/wrzfeijianshen/gui/gxui"
+	"github.com/wrzfeijianshen/gui/drivers/gl"
+)
+
+func appMain(driver gxui.Driver) {
+}
+func main() {
+	gl.StartDriver(appMain)
+}
